152f14d481ab5143dad8c30923eba86e85bbbab7
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-30 13:25:44 -0600

    Added the background code.

--- SampleGame/Controller/SpaceGame.cs
+++ SampleGame/Controller/SpaceGame.cs
@@ -33,6 +33,13 @@ namespace SampleGame.Controller
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
+		// Image used to display the static background
+		private Texture2D mainBackground;
+
+		// Parallaxing Layers
+		private ParallaxingBackground bgLayer1;
+		private ParallaxingBackground bgLayer2;
+
 		public SpaceGame()
 		{
 			graphics = new GraphicsDeviceManager(this);
@@ -52,6 +59,9 @@ namespace SampleGame.Controller
 
 			// Set a constant player move speed
 			playerMoveSpeed = 8.0f;
+
+			bgLayer1 = new ParallaxingBackground();
+			bgLayer2 = new ParallaxingBackground();
 
 			base.Initialize();
 		}
@@ -74,6 +84,12 @@ namespace SampleGame.Controller
 
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 			player.Initialize(playerAnimation, playerPosition);
+
+			// Load the parallaxing background
+			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
+
+			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
 		}
 
 		/// <summary>
@@ -104,6 +120,10 @@ namespace SampleGame.Controller
 
 			//Update the player
 			UpdatePlayer(gameTime);
+
+			// Update the parallaxing background
+			bgLayer1.Update();
+			bgLayer2.Update();
 		}
 
 		/// <summary>
@@ -117,7 +137,14 @@ namespace SampleGame.Controller
 			//TODO: Add your drawing code here
 
 			// Start drawing 
-			spriteBatch.Begin(); 
+			spriteBatch.Begin();
+
+			spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
+
+			// Draw the moving background
+			bgLayer1.Draw(spriteBatch);
+			bgLayer2.Draw(spriteBatch);
+
 			// Draw the Player 
 			player.Draw(spriteBatch); 
 			// Stop drawing 
